3e32a563b08c98ba9e6966ade97a9ba40de48fc1
MatBenfield <MatBenfield@users.noreply.github.com>
2022-08-04 20:21:03 +0100

    basics
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

---
+++ scripts/index.py
@@ -0,0 +1,16 @@
+"""
+Script to do a thing
+"""
+# Imports
+
+
+# Methods
+def print_helloworld():
+    """
+    Prints "Hello World"
+    """
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    print_helloworld()
